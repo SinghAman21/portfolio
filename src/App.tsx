9acaf3699d6614d02a283194f2fb5b43a6bd88a9
SinghAman21 <155352431+SinghAman21@users.noreply.github.com>
2025-02-21 20:48:58 +0530

    ascii added

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import AsciiArt from './AsciiArt';
+// import LetterGlitch from './LetterGlitch'
+// import AsciiArt from './AsciiArt'
+
+function App() {
+  return (
+    <>
+      {/* <div style={{height:50, width:110, margin:20}}>                    <OutBox>new coneten .</OutBox></div> */}
+      {/* <LetterGlitch
+        glitchSpeed={50}
+        centerVignette={false}
+        outerVignette={true}
+        smooth={true}
+      >
+      </LetterGlitch> */}
+<AsciiArt></AsciiArt>
+</>
+  );
+};
+
+
+export default App
