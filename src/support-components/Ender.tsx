d71aa52bea1ddacfe9b10e4e8875da95f00b019f
SinghAman21 <singhaman1825@gmail.com>
2025-06-29 16:57:47 +0530

    ghpages

--- src/support-components/Ender.tsx
+++ src/support-components/Ender.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { SiPeerlist } from 'react-icons/si';
 import { AiOutlineMail } from 'react-icons/ai';
+// import { AiOutlineMail } from 'react-icons/ai';
 
 const Footer: React.FC = () => {
   return (
